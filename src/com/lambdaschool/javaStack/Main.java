2ef344589af4d91ee8278d3b2f4b4875365b1f44
JasonCorp84 <csababalogh84@gmail.com>
2019-02-07 13:31:18 -0800

    LIFO complete

--- src/com/lambdaschool/javaStack/Main.java
+++ src/com/lambdaschool/javaStack/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Stack myStack =  new Stack(1000);
+        myStack.numOfItems();
+        myStack.push("item1");
+        myStack.push("item2");
+        myStack.push("item3");
+        myStack.numOfItems();
+        myStack.print();
+        myStack.pop();
+        myStack.print();
+        myStack.pop();
+        myStack.print();
+        myStack.pop();
+        myStack.print();
+        myStack.numOfItems();
     }
 }
--- src/com/lambdaschool/javaStack/Stack.java
+++ src/com/lambdaschool/javaStack/Stack.java
@@ -1,10 +1,80 @@
 package com.lambdaschool.javaStack;
 
+import java.util.Arrays;
+
 public class Stack
 {
     // LIFO Functionality
-    int[] intArray = new int[1000];
+    int maxSize;
+    int top;
+    String arr[];
+    String textToPop;
+    String[] current;
 
+    public Stack(int n)
+    {
+        maxSize = n;
+        arr =  new String[maxSize];
+        top = 0;
+    }
 
+    public void print()
+    {
+        if(top == 0)
+        {
+            System.out.println("Your stack is empty");
+        } else
+        {
+            for (int i = 0; i < top; i++)
+            {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+    public String pop()
+    {
+        if(top == 0)
+        {
+            System.out.println("your stack is already empty");
+        } else
+        {
+            textToPop = arr[top];
+            arr[top] = null;
+            top--;
+        }
+        return textToPop;
+    }
+    public void push(String str)
+    {
+        if(top > 1000)
+        {
+            System.out.println("let's not do that, we don't have enough room");
+        } else
+        {
+            arr[top] = str;
+            top++;
+        }
 
+    }
+    public void numOfItems()
+    {
+        if(top == 0)
+        {
+            System.out.printf("There are no items in your Stack");
+        }
+        else
+        {
+            System.out.println("'" + top +"'");
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Stack{" +
+                "maxSize=" + maxSize +
+                ", top=" + top +
+                ", arr=" + Arrays.toString(arr) +
+                '}';
+    }
 }
